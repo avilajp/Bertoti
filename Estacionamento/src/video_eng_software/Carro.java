ab3a361d694948f677a2500a4bb128e9dd3c8fcb
João Pedro Ávila <89164277+avilajp@users.noreply.github.com>
2023-08-28 20:39:10 -0300

    Estacionamento

---
+++ Estacionamento/src/video_eng_software/Carro.java
@@ -0,0 +1,27 @@
+package video_eng_software;
+
+public class Carro {
+	private String placa; 
+	private Especificacao espec;
+	
+	
+	
+	public Carro(String placa, Especificacao espec) {
+		this.placa = placa;
+		this.espec = espec;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public Especificacao getEspec() {
+		return espec;
+	}
+	public void setEspec(Especificacao espec) {
+		this.espec = espec;
+	}
+	
+	
+}
